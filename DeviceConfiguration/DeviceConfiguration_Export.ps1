b95695517e8e7d644f1bbbf91d97c339cd2f7736
Henry Wang <henry_ly@hotmail.com>
2023-03-22 17:48:05 +0800

    Replace ADAL with MSAL for user authentication.

--- DeviceConfiguration/DeviceConfiguration_Export.ps1
+++ DeviceConfiguration/DeviceConfiguration_Export.ps1
@@ -9,144 +9,106 @@ See LICENSE in the project root for license information.
 
 ####################################################
 
+## 20230322 - Replace Get-AuthToken function using MSAL. The original script is using ADAL
 function Get-AuthToken {
 
-<#
-.SYNOPSIS
-This function is used to authenticate with the Graph API REST interface
-.DESCRIPTION
-The function authenticate with the Graph API Interface with the tenant name
-.EXAMPLE
-Get-AuthToken
-Authenticates you with the Graph API interface
-.NOTES
-NAME: Get-AuthToken
-#>
-
-[cmdletbinding()]
-
-param
-(
-    [Parameter(Mandatory=$true)]
-    $User
-)
-
-$userUpn = New-Object "System.Net.Mail.MailAddress" -ArgumentList $User
-
-$tenant = $userUpn.Host
-
-Write-Host "Checking for AzureAD module..."
-
-    $AadModule = Get-Module -Name "AzureAD" -ListAvailable
-
-    if ($AadModule -eq $null) {
-
-        Write-Host "AzureAD PowerShell module not found, looking for AzureADPreview"
-        $AadModule = Get-Module -Name "AzureADPreview" -ListAvailable
-
+    <#
+    .SYNOPSIS
+    This function is used to authenticate with the Graph API REST interface
+    .DESCRIPTION
+    The function authenticate with the Graph API Interface with the tenant name
+    .EXAMPLE
+    Get-AuthToken
+    Authenticates you with the Graph API interface
+    .NOTES
+    NAME: Get-AuthToken
+    #>
+    
+    [cmdletbinding()]
+    
+    param
+    (
+        [Parameter(Mandatory=$true)]
+        $User
+    )
+    
+    $userUpn = New-Object "System.Net.Mail.MailAddress" -ArgumentList $User
+    
+    $tenant = $userUpn.Host
+    
+    Write-Host "Checking for AzureAD module..."
+    
+    try {
+        Write-Host "Get Microsoft.Graph.Authentication - Start"
+        $AadModule = Get-Module -Name "Microsoft.Graph.Authentication" -ListAvailable -ErrorAction Stop
+        import-module Microsoft.Graph.Authentication -RequiredVersion 1.23.0
+        Write-Host "Get Microsoft.Graph.Authentication - End"
     }
-
-    if ($AadModule -eq $null) {
-        write-host
-        write-host "AzureAD Powershell module not installed..." -f Red
-        write-host "Install by running 'Install-Module AzureAD' or 'Install-Module AzureADPreview' from an elevated PowerShell prompt" -f Yellow
-        write-host "Script can't continue..." -f Red
-        write-host
+    catch {
+        Write-Host "Microsoft.Graph.Authentication PowerShell module not found." -f Red
+        Write-Host "Install by running 'Install-Module Microsoft.Graph.Authentication' from an elevated PowerShell prompt" -f Yellow
+        Write-Host "Script can't continue..." -f Red
         exit
     }
+    
+    $clientId = "d1ddf0e4-d672-4dae-b554-9d5bdfd93547"
+    [String[]] $scopes = @("user.read","DeviceManagementServiceConfig.ReadWrite.All")
+    $redirectUri = "urn:ietf:wg:oauth:2.0:oob"
+    
+    $authority = "https://login.microsoftonline.com/$Tenant"
+    
+        try {
+      ##      $app = [Microsoft.Identity.Client.PublicClientApplication]::new($clientId, $authority)
+            Write-Host "Building application context..."
+            $app1 = [Microsoft.Identity.Client.PublicClientApplicationBuilder]::Create($clientId)
+            Write-Host "Building application context...1"
+            $app1.WithAuthority($authority)
+            $app1.WithRedirectUri($redirectUri)
+            $app = $app1.Build()
 
-# Getting path to ActiveDirectory Assemblies
-# If the module count is greater than 1 find the latest version
 
-    if($AadModule.count -gt 1){
-
-        $Latest_Version = ($AadModule | select version | Sort-Object)[-1]
-
-        $aadModule = $AadModule | ? { $_.version -eq $Latest_Version.version }
-
-            # Checking if there are multiple versions of the same module found
-
-            if($AadModule.count -gt 1){
-
-            $aadModule = $AadModule | select -Unique
-
+            $authResult = $app.AcquireTokenInteractive($scopes).ExecuteAsync().Result
+    
+            # If the accesstoken is valid then create the authentication header
+    
+            if($authResult.AccessToken){
+    
+            # Creating header for Authorization token
+    
+            $authHeader = @{
+                'Content-Type'='application/json'
+                'Authorization'="Bearer " + $authResult.AccessToken
+                'ExpiresOn'=$authResult.ExpiresOn
+                }
+            
+            $global:authToken = $authHeader
+                
+            return 0
+    
             }
-
-        $adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
-        $adalforms = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.Platform.dll"
-
-    }
-
-    else {
-
-        $adal = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
-        $adalforms = Join-Path $AadModule.ModuleBase "Microsoft.IdentityModel.Clients.ActiveDirectory.Platform.dll"
-
-    }
-
-[System.Reflection.Assembly]::LoadFrom($adal) | Out-Null
-
-[System.Reflection.Assembly]::LoadFrom($adalforms) | Out-Null
-
-$clientId = "d1ddf0e4-d672-4dae-b554-9d5bdfd93547"
-
-$redirectUri = "urn:ietf:wg:oauth:2.0:oob"
-
-$resourceAppIdURI = "https://graph.microsoft.com"
-
-$authority = "https://login.microsoftonline.com/$Tenant"
-
-    try {
-
-    $authContext = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext" -ArgumentList $authority
-
-    # https://msdn.microsoft.com/en-us/library/azure/microsoft.identitymodel.clients.activedirectory.promptbehavior.aspx
-    # Change the prompt behaviour to force credentials each time: Auto, Always, Never, RefreshSession
-
-    $platformParameters = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.PlatformParameters" -ArgumentList "Auto"
-
-    $userId = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.UserIdentifier" -ArgumentList ($User, "OptionalDisplayableId")
-
-    $authResult = $authContext.AcquireTokenAsync($resourceAppIdURI,$clientId,$redirectUri,$platformParameters,$userId).Result
-
-        # If the accesstoken is valid then create the authentication header
-
-        if($authResult.AccessToken){
-
-        # Creating header for Authorization token
-
-        $authHeader = @{
-            'Content-Type'='application/json'
-            'Authorization'="Bearer " + $authResult.AccessToken
-            'ExpiresOn'=$authResult.ExpiresOn
+    
+            else {
+    
+            Write-Host
+            Write-Host "Authorization Access Token is null, please re-run authentication..." -ForegroundColor Red
+            Write-Host
+            break
+    
             }
-
-        return $authHeader
-
+    
         }
-
-        else {
-
-        Write-Host
-        Write-Host "Authorization Access Token is null, please re-run authentication..." -ForegroundColor Red
-        Write-Host
+    
+        catch {
+    
+        write-host $_.Exception.Message -f Red
+        write-host $_.Exception.ItemName -f Red
+        write-host
         break
-
+    
         }
-
+    
     }
-
-    catch {
-
-    write-host $_.Exception.Message -f Red
-    write-host $_.Exception.ItemName -f Red
-    write-host
-    break
-
-    }
-
-}
-
+    
 ####################################################
 
 Function Get-DeviceConfigurationPolicy(){
@@ -297,7 +259,7 @@ if($global:authToken){
 
             }
 
-        $global:authToken = Get-AuthToken -User $User
+        Get-AuthToken -User $User
 
         }
 }
@@ -314,7 +276,7 @@ else {
     }
 
 # Getting the authorization token
-$global:authToken = Get-AuthToken -User $User
+Get-AuthToken -User $User
 
 }
 
